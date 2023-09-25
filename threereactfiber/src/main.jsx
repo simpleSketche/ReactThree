4fbc97d19a649646c86a509b3aa9c081c68ab69d
SimpleSketch_Yankun <yangyankun.work@gmail.com>
2023-09-25 10:26:15 -0400

    set up r3f

---
+++ threereactfiber/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM, { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <div>
+    <App>
+
+    </App>
+  </div>
+)
